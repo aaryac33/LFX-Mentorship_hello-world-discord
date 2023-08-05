e4c217a413438b99056741ac4882b6131b32d1f7
aaryac33 <102223660+aaryac33@users.noreply.github.com>
2023-08-06 00:24:33 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serenity::{
     async_trait,
-    model::gateway::Ready,
+    model::channel::Message,
     prelude::*,
 };
 use anyhow::Result;
@@ -9,9 +9,51 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn message(&self, ctx: Context, msg: Message) {
+        // Check if the message is from a bot or in a channel (not a DM)
+        if msg.author.bot || msg.is_private() {
+            return;
+        }
+
+        // Extract the question from the message content
+        let question = &msg.content;
+
+        // Send the question to the flow function-backed HTTP service
+        let response = match send_question_to_http_service(question).await {
+            Ok(answer) => answer,
+            Err(_) => "Error processing the question.".to_string(),
+        };
+
+        // Send the response to the user
+        if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+            eprintln!("Error sending message: {:?}", why);
+        }
     }
+}
+
+async fn send_question_to_http_service(question: &str) -> Result<String> {
+    // Replace the URL below with the URL of your flow function-backed HTTP service
+    let url = "https://example.com/ask";
+    let client = reqwest::Client::new();
+    let response = client
+        .post(url)
+        .json(&json!({
+            "question": question,
+        }))
+        .send()
+        .await?;
+
+    let body = response.text().await?;
+    let json_response: serde_json::Value = serde_json::from_str(&body)?;
+
+    // Assuming the response contains an "answer" field in JSON
+    if let Some(answer) = json_response.get("answer") {
+        if let Some(answer_str) = answer.as_str() {
+            return Ok(answer_str.to_string());
+        }
+    }
+
+    Ok("Error: Invalid response from the HTTP service.".to_string())
 }
 
 #[tokio::main]
